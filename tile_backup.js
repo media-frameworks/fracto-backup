cdfa4fa2ff02c392ff89f6ebc187fb93d1b3b447
media-frameworks <mikehallvideo@gmail.com>
2025-11-23 12:59:46 -0500

    removing tiles needing update after update

--- tile_backup.js
+++ tile_backup.js
@@ -56,42 +56,44 @@ const backup_short_codes = (short_codes, starting_index, cb) => {
    });
 }
 
-const backup_level_short_codes = (level_short_codes) => {
+const backup_level_short_codes = (level_short_codes, cb) => {
    if (!level_short_codes.length) {
       console.log('complete')
+      cb()
       return
    }
    const short_codes = level_short_codes.pop()
    const level = short_codes[0].length
    console.log(`backing up level ${level} (${short_codes.length} tiles)`)
    backup_short_codes(short_codes, 0, () => {
-      backup_level_short_codes(level_short_codes)
+      backup_level_short_codes(level_short_codes, cb)
    })
 }
 
-FractoIndexedTiles.load_short_codes('needs_update', short_codes => {
-   short_codes.forEach((short_code, index) => {
-      const level = short_code.length
-      const remaining = short_codes.length - index
-      if (remaining % 10000 === 0) {
-         console.log(`updates: [${level}] ${remaining} remain`)
-      }
-      const naught = level < 10 ? '0' : ''
-      const level_dirname = `L${naught}${level}`
-      const tiles_dir = "./tiles";
-      const level_dir = `${tiles_dir}/${level_dirname}`
-      const filename = `${level_dir}/${short_code}.gz`
-      if (fs.existsSync(filename)) {
-         console.log(`update needed: ${short_code}`)
-         fs.unlinkSync(filename)
-      }
-   })
-   FractoIndexedTiles.load_short_codes('indexed', short_codes => {
-      const level_short_codes = []
-      for (let level = 30; level > 2; level--) {
-         const short_codes_for_level = short_codes.filter(short_code => short_code.length === level)
-         level_short_codes.push(short_codes_for_level)
-      }
-      backup_level_short_codes(level_short_codes)
+FractoIndexedTiles.load_short_codes('indexed', short_codes => {
+   const level_short_codes = []
+   for (let level = 30; level > 2; level--) {
+      const short_codes_for_level = short_codes.filter(short_code => short_code.length === level)
+      level_short_codes.push(short_codes_for_level)
+   }
+   backup_level_short_codes(level_short_codes, () => {
+      FractoIndexedTiles.load_short_codes('needs_update', short_codes => {
+         short_codes.forEach((short_code, index) => {
+            const level = short_code.length
+            const remaining = short_codes.length - index
+            if (remaining % 10000 === 0) {
+               console.log(`updates: [${level}] ${remaining} remain`)
+            }
+            const naught = level < 10 ? '0' : ''
+            const level_dirname = `L${naught}${level}`
+            const tiles_dir = "./tiles";
+            const level_dir = `${tiles_dir}/${level_dirname}`
+            const filename = `${level_dir}/${short_code}.gz`
+            if (fs.existsSync(filename)) {
+               console.log(`update needed: ${short_code}`)
+               fs.unlinkSync(filename)
+            }
+         })
+      })
    })
 })
